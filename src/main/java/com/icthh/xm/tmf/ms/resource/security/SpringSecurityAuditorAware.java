3f23dc2f03c9f0912b6a11b74be8bf340b81c96c
dtsarenko <dimatsarenko89@gmail.com>
2019-03-12 12:39:52 +0200

    org import

--- src/main/java/com/icthh/xm/tmf/ms/resource/security/SpringSecurityAuditorAware.java
+++ src/main/java/com/icthh/xm/tmf/ms/resource/security/SpringSecurityAuditorAware.java
@@ -1,9 +1,7 @@
 package com.icthh.xm.tmf.ms.resource.security;
 
 import com.icthh.xm.tmf.ms.resource.config.Constants;
-
 import java.util.Optional;
-
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
